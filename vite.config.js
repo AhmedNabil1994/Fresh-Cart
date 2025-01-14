a930c0b7dffb4baab8251e1531ad5d4be86a4b44
AhmedNabil1994 <94ahmed.nabil@gmail.com>
2025-01-14 18:54:35 +0200

    edit

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,8 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/",
-  build: {
-    outDir: "dist",
-  },
   plugins: [react()],
-  assetsInclude: ["**/*.JPG"],
-});
+  assetsInclude: ['**/*.JPG'],
+})
