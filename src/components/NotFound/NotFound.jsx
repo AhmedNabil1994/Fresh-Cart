b0942ee1a6ff48b7c54613f71436873319079222
AhmedNabil1994 <94ahmed.nabil@gmail.com>
2025-01-15 17:54:49 +0200

    add NotFound component design

--- src/components/NotFound/NotFound.jsx
+++ src/components/NotFound/NotFound.jsx
@@ -6,7 +6,10 @@ import style from "./NotFound.module.css";
 export default function NotFound() {
   return (
     <>
-     <section className={style.NotFound}>NotFound</section>
+     <section className="text-center py-48 ">
+      <h2 className="font-medium text-4xl sm:text-6xl md:text-8xl lg:text-[110px]">404 Not Found</h2>
+      <p className="mt-4">Your visited page not found.</p>
+     </section>
     </>
   );
 }
