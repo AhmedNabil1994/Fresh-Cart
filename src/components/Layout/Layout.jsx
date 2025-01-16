efd69ded5b186eee231e6422f96daad3babdac61
AhmedNabil1994 <94ahmed.nabil@gmail.com>
2025-01-16 14:45:37 +0200

    edit layout style in md

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -8,7 +8,7 @@ export default function Layout() {
   return (
     <>
       <Navbar />
-      <section className="py-20 px-4">
+      <section className="px-4 md:py-32 lg:py-20">
         <div className="container mx-auto">
           <Outlet />
         </div>
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -6,8 +6,8 @@ import { IoCartOutline } from "react-icons/io5";
 export default function NavbarComp() {
   return (
     <>
-      <nav className="bg-white border-gray-200 border-b fixed top-0 inset-x-0 z-50">
-        <div className="gap-y-4 max-w-screen-xl flex flex-wrap items-center justify-center min-400:justify-between md:justify-evenly lg:justify-between mx-auto p-4">
+      <nav className="bg-white border-b border-gray-200  fixed top-0 inset-x-0 z-50">
+        <div className="gap-y-4 max-w-screen-xl flex flex-wrap items-center justify-center min-400:justify-between md:justify-evenly mx-auto p-4">
           <Link
             to="/"
             className="flex items-center space-x-3 rtl:space-x-reverse"
