2590f460620526372b58c6e1ccf00d5acac8739d
AhmedNabil1994 <94ahmed.nabil@gmail.com>
2025-01-27 02:20:06 +0200

    solve loader height problem

--- src/components/shared/Loader/Loader.jsx
+++ src/components/shared/Loader/Loader.jsx
@@ -3,7 +3,7 @@ import style from "./Loader.module.css";
 
 export default function Loader() {
   return (
-    <div className="h-[100vh] flex justify-center items-center fixed inset-0 bg-white z-40">
+    <div className="flex justify-center items-center fixed inset-0 bg-white z-40">
       <div className={`${style.loader}`}></div>
     </div>
   );
