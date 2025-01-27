26255fe2383b287e8ec69566149e957561182712
AhmedNabil1994 <94ahmed.nabil@gmail.com>
2025-01-28 00:36:32 +0200

    add to cart in product details-remove react-toastify-comment styling in details

--- src/components/Products/Product/Product.jsx
+++ src/components/Products/Product/Product.jsx
@@ -2,7 +2,7 @@ import { useContext, useState } from "react";
 import { Link } from "react-router-dom";
 import StarRatings from "react-star-ratings";
 import { CartContext } from "../../../context/CartContext";
-import { toast } from "react-hot-toast";
+import  toast  from "react-hot-toast";
 // css module
 // import style from "./Product.module.css";
 
@@ -13,7 +13,7 @@ export default function Product({ product }) {
   */
   const { addToCart } = useContext(CartContext);
 
-  const handleAddToCart = async (e, id) => {
+  const handleClick = async (e, id) => {
     e.preventDefault();
     e.stopPropagation();
     const toastId = toast.loading("Adding product to cart...");
@@ -32,6 +32,7 @@ export default function Product({ product }) {
           id: toastId,
         });
   };
+
   return (
     <>
       <div className="w-full sm:w-6/12 md:w-4/12 lg:w-3/12 mb-[60px] group px-[15px]">
@@ -45,8 +46,8 @@ export default function Product({ product }) {
                 className="p-8"
               />
               <button
-                onClick={(e) => handleAddToCart(e, product.id)}
-                className="absolute inset-x-0 bottom-0 capitalize bg-black text-white text-center w-full py-2 opacity-0 translate-y-full group-hover:opacity-100 group-hover:translate-y-0 transition duration-500"
+                onClick={(e) => handleClick(e, product.id)}
+                className="absolute inset-x-0 bottom-0 capitalize bg-black text-white text-center w-full py-2 opacity-0 translate-y-full group-hover:opacity-100 group-hover:translate-y-0 hover:bg-secondary transition duration-500"
               >
                 add to cart
               </button>
--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -1,8 +1,6 @@
 import axios from "axios";
 import { createContext } from "react";
 import Cookies from "js-cookie";
-import { toast } from "react-hot-toast";
-
 
 export const CartContext = createContext();
 
@@ -10,13 +8,13 @@ export default function CartContextProvider({ children }) {
   const headers = { token: Cookies.get("token") };
 
   const addToCart = async (productId) => {
-      return await axios
+    return await axios
       .post(
-          `https://ecommerce.routemisr.com/api/v1/cart`,
-          { productId },
-          { headers }
-        )
-        // toast.loading("Adding to cart")
+        `https://ecommerce.routemisr.com/api/v1/cart`,
+        { productId },
+        { headers }
+      )
+      // toast.loading("Adding to cart")
       .then(({ data }) => data)
       .catch((error) => error);
   };
