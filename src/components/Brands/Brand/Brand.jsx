18dc6c8769bd86857c3d5f7e94d26d451081c420
AhmedNabil1994 <94ahmed.nabil@gmail.com>
2025-02-01 17:05:05 +0200

    create brand component

---
+++ src/components/Brands/Brand/Brand.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+
+// css module
+// import style from "./Brand.module.css";
+
+export default function Brand() {
+  return (
+    <>
+      <section>Brand</section>
+    </>
+  );
+}
