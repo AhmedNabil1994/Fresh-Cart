fc7673468f441ec19abf0091ca2ffc39b6fbc3d7
AhmedNabil1994 <94ahmed.nabil@gmail.com>
2025-02-01 22:52:07 +0200

    add brands design and logic

--- src/components/Brands/Brand/Brand.jsx
+++ src/components/Brands/Brand/Brand.jsx
@@ -1,12 +1,23 @@
-import { useState } from "react";
+// import { useState } from "react";
 
 // css module
 // import style from "./Brand.module.css";
 
-export default function Brand() {
+export default function Brand({brand}) {
   return (
     <>
-      <section>Brand</section>
+      <div className="w-full sm:w-6/12 md:w-4/12 lg:w-3/12 mb-[60px] group px-[15px]">
+          <div className="">
+            <img
+              src={brand.image}
+              alt={brand.name}
+              className="transition duration-700 w-full"
+            />
+          </div>
+        <h2 className="text-center mt-2 group-hover:text-secondary font-semibold transition duration-500">
+          {brand.name}
+        </h2>
+      </div>
     </>
   );
 }
--- src/components/Brands/Brands.jsx
+++ src/components/Brands/Brands.jsx
@@ -1,12 +1,52 @@
 import React from "react";
 // import style from "./Brands.module.css";
 import Brand from "../Brands/Brand/Brand";
+import axios from "axios";
+import { useQuery } from "@tanstack/react-query";
+import Loader from "../shared/Loader/Loader";
+import ApiError from "../shared/ApiError/ApiError";
+import SectionHeader from "../shared/SectionHeader/SectionHeader";
 
 export default function Brands() {
+  const getBrands = () => {
+    return axios.get(`https://ecommerce.routemisr.com/api/v1/brands`);
+  };
+
+  const {
+    data: brands,
+    error,
+    isError,
+    isLoading,
+  } = useQuery({
+    queryKey: ["allBrands"],
+    queryFn: getBrands,
+    select: (brands) => brands.data.data,
+  });
+  console.log(brands, "all brands ");
+
   return (
     <>
-      {/* <h1>Brands</h1> */}
-      <Brand />
+      <>
+        {isLoading ? (
+          <Loader />
+        ) : isError ? (
+          <ApiError error={error.response.data.message} />
+        ) : (
+          brands && (
+            <>
+              <SectionHeader
+                title="Brands"
+                subtitle="Explore Our All Brands"
+              />
+              <section className="row mx-[-15px]">
+                {brands.map((brand) => (
+                  <Brand key={brand._id} brand={brand} />
+                ))}
+              </section>
+            </>
+          )
+        )}
+      </>
     </>
   );
 }
