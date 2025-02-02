f1f91022fa978263aa8ed2e919eabaea9a41b100
AhmedNabil1994 <94ahmed.nabil@gmail.com>
2025-02-02 03:09:55 +0200

    search to be completed

--- src/components/Products/RecentProducts/RecentProducts.jsx
+++ src/components/Products/RecentProducts/RecentProducts.jsx
@@ -4,12 +4,18 @@ import Loader from "../../shared/Loader/Loader";
 import ApiError from "../../shared/ApiError/ApiError";
 import { useQuery } from "@tanstack/react-query";
 import { Link } from "react-router-dom";
+import Search from "../../Search/Search";
+import { useEffect, useState } from "react";
 // css module
 // import style from "./RecentProducts.module.css";
 
 export default function RecentProducts() {
+  const [allProducts, setAllProducts] = useState([]);
+
   const getRecentProducts = () => {
-    return axios.get(`https://ecommerce.routemisr.com/api/v1/products?limit=20`);
+    return axios.get(
+      `https://ecommerce.routemisr.com/api/v1/products?limit=20`
+    );
   };
 
   const {
@@ -24,25 +30,32 @@ export default function RecentProducts() {
   });
   // console.log(products,"recent products");
 
+  useEffect(() => {
+    if (products) setAllProducts(products);
+  }, [products]);
+
   return (
     <>
+      <Search allProducts={allProducts} setAllProducts={setAllProducts} />
       {isLoading ? (
         <Loader />
       ) : isError ? (
         <ApiError error={error.response.data.message} />
       ) : (
-        <>
-          <section className="row mx-[-15px]">
-            {products.map((product) => (
-              <Product product={product} key={product.id} />
-            ))}
-          </section>
-          <Link to="/products">
-            <button className="mx-auto block w-full md:w-1/3 lg:w-[30%] capitalize mt-2 rounded bg-secondary text-white px-12 py-4 font-medium hover:bg-opacity-90 transition-colors duration-500">
-              View All Products
-            </button>
-          </Link>
-        </>
+        allProducts && (
+          <>
+            <section className="row mx-[-15px]">
+              {allProducts.map((product) => (
+                <Product product={product} key={product.id} />
+              ))}
+            </section>
+            <Link to="/products">
+              <button className="mx-auto block w-full md:w-1/3 lg:w-[30%] capitalize mt-2 rounded bg-secondary text-white px-12 py-4 font-medium hover:bg-opacity-90 transition-colors duration-500">
+                View All Products
+              </button>
+            </Link>
+          </>
+        )
       )}
     </>
   );
